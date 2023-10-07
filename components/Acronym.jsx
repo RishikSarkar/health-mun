4d124ac219efc16c22e7239b6fe124fcf0ebfbe2
Rishik Sarkar <rishiksarkar123@gmail.com>
2023-10-06 23:28:31 -0400

    updated acronym page

--- components/Acronym.jsx
+++ components/Acronym.jsx
@@ -20,21 +20,22 @@ const Acronym = () => {
         <div id='countdown' className='font-marcellus w-full h-screen text-center bg-[#F7FAFA]'>
             <div className='max-w-[1440px] w-full h-full mx-auto flex flex-col justify-center items-center'>
                 <div className='p-8 text-2xl uppercase flex'>
-                    What we stand for:
+                    What we stand for
                 </div>
 
-                <div className='flex justify-center items-center p-8'>
-                    {Array.from('Health').map((letter, index) => (
-                        <div
-                            key={index}
-                            className='bg-white text-3xl uppercase hover:bg-gray-200 text-black font-bold py-2 px-4 rounded cursor-pointer'
-                            onClick={() => handleClick(letter)}
-                        >
-                            {letter}
-                        </div>
-                    ))}
-
-                    <div className='w-[400px] h-auto text-2xl'>
+                <div className='bg-white p-8'>
+                    <div className='flex justify-center items-center p-2 pb-4'>
+                        {Array.from('Health').map((letter, index) => (
+                            <div
+                                key={index}
+                                className='bg-white text-3xl uppercase hover:bg-[#CADEDF] text-black font-bold py-2 px-4 cursor-pointer'
+                                onClick={() => handleClick(letter)}
+                            >
+                                {letter}
+                            </div>
+                        ))}
+                    </div>
+                    <div className='w-[400px] h-auto text-2xl p-2 pt-4'>
                         {currWord}
                     </div>
                 </div>
