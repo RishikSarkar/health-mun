378b757f426671a534d830e173b9084c00b53ca4
Rishik Sarkar <rishiksarkar123@gmail.com>
2024-01-17 12:59:06 -0500

    optimized seo 1

--- pages/index.js
+++ pages/index.js
@@ -1,24 +1,31 @@
 import Head from 'next/head';
+import { NextSeo } from 'next-seo';
 import Main from '../components/Main.jsx'
 import Countdown from '../components/Countdown.jsx';
 import Acronym from '../components/Acronym.jsx';
 import FAQ from '../components/FAQ.jsx';
 import Contact from '../components/Contact.jsx';
-
 import { ParallaxProvider } from 'react-scroll-parallax';
 
 export default function Home() {
   return (
-    <div className>
-      <Head>
-        <title>HealthMUN</title>
-        <meta
-          name="description"
-          content="HealthMUN, the world's first-ever Model United Nations experience organized for pre-health students. See you in 2024!"
-        />
-        <link rel="icon" href="/favicon/favicon.ico" />
-      </Head>
-
+    <div>
+      <NextSeo
+        title="HealthMUN"
+        description="HealthMUN, the world's first-ever Model United Nations experience organized for pre-health students. See you in 2024!"
+        openGraph={{
+          url: 'http://www.healthmun.org/',
+          title: 'HealthMUN',
+          description: "HealthMUN, the world's first-ever Model United Nations experience organized for pre-health students. See you in 2024!",
+          site_name: 'HealthMUN',
+        }}
+        additionalLinkTags={[
+          {
+            rel: 'icon',
+            href: '/favicon/favicon.ico',
+          },
+        ]}
+      />
       <ParallaxProvider>
         <Main />
       </ParallaxProvider>
